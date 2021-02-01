bac9061c74a895270f1f11d167f423c637f722d2
Adil Hussain <>
2021-02-01 13:45:22 +0000

    (app1) Created an application which demonstrates a problem with the Android 11 emulator. See the `UiAutomatorTest` class.

---
+++ app1/src/androidTest/java/com/tazkiyatech/uiautomator/app1/UiAutomatorTest.kt
@@ -0,0 +1,19 @@
+package com.tazkiyatech.uiautomator.app1
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+class UiAutomatorTest {
+
+    private val uiDevice: UiDevice
+        get() = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    @Test
+    fun launcherPackageName() {
+        assertNotEquals("com.android.settings", uiDevice.launcherPackageName)
+        assertEquals("com.google.android.apps.nexuslauncher", uiDevice.launcherPackageName)
+    }
+}
