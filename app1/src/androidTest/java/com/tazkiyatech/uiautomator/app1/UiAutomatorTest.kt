048cf7c377bd1e3e51c81ec563e4aaeb7a920ed8
Adil Hussain <adilson05uk@gmail.com>
2021-06-15 12:30:23 +0100

    Added a couple more test methods in `UiAutomatorTest` to demonstrate why the Android 11 emulator reporting its launcher package as "com.android.settings" when that is not its launcher package is problematic.

--- app1/src/androidTest/java/com/tazkiyatech/uiautomator/app1/UiAutomatorTest.kt
+++ app1/src/androidTest/java/com/tazkiyatech/uiautomator/app1/UiAutomatorTest.kt
@@ -1,8 +1,12 @@
 package com.tazkiyatech.uiautomator.app1
 
+import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import org.junit.Assert.assertNotEquals
+import org.junit.Assert.fail
 import org.junit.Test
 
 class UiAutomatorTest {
@@ -13,5 +17,45 @@ class UiAutomatorTest {
     @Test
     fun launcherPackageName() {
         assertNotEquals("com.android.settings", uiDevice.launcherPackageName)
+    }
+
+    @Test
+    fun waitOnLauncher() {
+        // When.
+        uiDevice.pressHome()
+
+        // Then.
+        val hasObject = uiDevice.wait(Until.hasObject(By.pkg(uiDevice.launcherPackageName)), 10_000L)
+
+        if (!hasObject) {
+            fail("Waited on the launcher but it did not show")
+        }
+    }
+
+    @Test
+    fun waitOnLauncher_with_hacked_launcher_package_name() {
+        // When.
+        uiDevice.pressHome()
+
+        // Then.
+        val hasObject = uiDevice.wait(Until.hasObject(By.pkg(getHackedLauncherPackageName())), 10_000L)
+
+        if (!hasObject) {
+            fail("Waited on the launcher but it did not show")
+        }
+    }
+
+    /**
+     * This method exists as an alternative to the [UiDevice.getLauncherPackageName] method
+     * because that method returns `"com.android.settings"` (incorrectly) when run in an Android 11 emulator device.
+     *
+     * @return the package name of the device's default launcher (a.k.a. home) app.
+     */
+    private fun getHackedLauncherPackageName(): String? {
+        return if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R && uiDevice.productName.startsWith("sdk_gphone_")) {
+            "com.google.android.apps.nexuslauncher"
+        } else {
+            uiDevice.launcherPackageName
+        }
     }
 }
